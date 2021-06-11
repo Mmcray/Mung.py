38b2395523b3a993c4d4996fcbb9234c4d6b04b3
Michael McRay <mmcray5@gmail.com>
2021-06-11 17:24:00 -0500

    Fixed some formatting and logic issues/bugs. Can hopefully now continue adding Dash functionality.

--- file_loader.py
+++ file_loader.py
@@ -14,69 +14,69 @@ def read_file():
     exten = ('.' + str(extension_sp[1]))
     print('Extension to read is: ' + exten)
     read_extension = available.get(exten)
-# Loads the dataframe and saves it as 'df'
+    # Loads the dataframe and saves it as 'df'
     call_pandas = read_extension
     df = call_pandas(path + file_name)
     print("File has been loaded to Pandas.")
     return df
 
 
-class Mung:
-    # "Because the data won't Mung itself"
+df_r = read_file()
 
-    df = read_file()
-    # Takes an input as the file to be loaded into Pandas and the current working directory for that file location
-    def __init__(self, df):
-        self.df = df
 
-    # Pandas exploration methods
-    def data_info(self):
-        self.df = Mung.df
-        data_head = self.df.head()
-        data_description = self.df.describe()
-        data_nulls = self.df.isna().sum()
-        data_info = self.df.info(verbose=True)
-        data_uniq = self.df.nunique(axis=0)
+# Pandas exploration methods
+def data_info():
+    df = df_r
+    data_head = df.head()
+    data_description = df.describe()
+    data_nulls = df.isna().sum()
+    data_info = df.info(verbose=True)
+    data_uniq = df.nunique(axis=0)
 
-        print('\n First 5 rows:')
-        print(data_head)
-        print('\n DataFrame Description: ')
-        print(data_description)
-        print('\n DataFrame Info: ')
-        print(data_info)
-        print('\n Nulls: ')
-        print(data_nulls)
-        print('\n Unique values per column:')
-        print(data_uniq)
+    print('\n First 5 rows:')
+    print(data_head)
+    print('\n DataFrame Description: ')
+    print(data_description)
+    print('\n DataFrame Info: ')
+    print(data_info)
+    print('\n Nulls: ')
+    print(data_nulls)
+    print('\n Unique values per column:')
+    print(data_uniq)
 
-    def clean_nulls(self):
-        nonulls = ['Yes', 'yes', 'YES', 'y', 'Y']
-        question = input('Would you like to remove rows with Nulls (Yes or No)? ')
-        df = self.df
-        if question in nonulls:
-            bynulls = df.dropna()
-            print('Rows with Nulls have been removed. \n')
-            print(bynulls)
+
+def drop_cols():
+    col_lst_to_drop = []
+    no_drop = ['N', 'n', 'none']
+    df = df_r
+    col_lst = df.columns
+    for col in col_lst:
+        to_drop = input("List columns you'd like to drop one at a time. Input 'N' when done: ")
+        if to_drop in no_drop:
+            break
+        elif to_drop in col_lst:
+            col_lst_to_drop.append(to_drop)
+            continue
         else:
-            print('No rows will be dropped')
-        return df
+            print('Column not in dataframe, please try again.')
+            continue
+    dfd = df.drop(col_lst_to_drop, axis=1, inplace=False)
+    print('Done dropping columns')
+    print(col_lst_to_drop)
+    print(df.head())
+    print(dfd)
+    print(dfd.head())
+    return dfd
 
-    def drop_cols(self):
-        df = self.df
-        col_lst_to_drop = []
-        no_drop = ['N', 'n', 'none']
-        col_lst = df.columns
-        for col in col_lst:
-            to_drop = input("List columns you'd like to drop one at a time. Input 'N' when done: ")
-            if to_drop in no_drop:
-                break
-            elif to_drop in col_lst:
-                col_lst_to_drop.append(to_drop)
-                continue
-            else:
-                print('Column not in dataframe, please try again.')
-                continue
-        df = df.drop(col_lst_to_drop, axis='columns', inplace=True)
-        print('Done dropping columns')
-        return df
 
+def clean_nulls(dfd):
+    no_nulls = ['Yes', 'yes', 'YES', 'y', 'Y']
+    question = input('Would you like to remove rows with Nulls (Yes or No)? ')
+    if question in no_nulls:
+        df_dropped = dfd.dropna()
+        print('Rows with Nulls have been removed. \n')
+    else:
+        df_dropped = dfd
+        print('No rows will be dropped')
+    dfdn = df_dropped
+    return dfdn
--- main.py
+++ main.py
@@ -7,22 +7,32 @@ import dash_table
 import plotly.express as px
 import pandas as pd
 import file_loader as fl
+from file_loader import read_file
 
 app = dash.Dash(__name__)
 
-munging = fl.Mung(fl.read_file)
-munging.data_info()
-munging.clean_nulls()
-munging.drop_cols()
-df = munging.df
+
+def main():
+    df = read_file()
+    print('step1: Info')
+    fl.data_info()
+    print('step2: Clean Columns')
+    df_rc = fl.drop_cols()
+    print('step3: Clean Nulls')
+    df_rnc = fl.clean_nulls(df_rc)
+    return df_rnc
+
+
+dfg = main()
+print(dfg.head())
 
 figU = px.bar(
-    df.nunique(axis=0), x=df.columns, y=df.nunique(axis=0),
-    text=df.nunique(axis=0), title='Count of Uniques', )
+    dfg.nunique(axis=0), x=dfg.columns, y=dfg.nunique(axis=0),
+    text=dfg.nunique(axis=0), title='Count of Uniques', )
 
 figN = px.bar(
-    df.nunique(axis=0), x=df.columns, y=df.isna().sum(),
-    text=df.isna().sum(), title='Count of Nulls', )
+            dfg.nunique(axis=0), x=dfg.columns, y=dfg.isna().sum(),
+            text=dfg.isna().sum(), title='Count of Nulls', )
 
 app.layout = html.Div(
     [
@@ -31,8 +41,8 @@ app.layout = html.Div(
         dash_table.DataTable(
             id='table',
             columns=[{"name": i, "id": i}
-                     for i in df.columns],
-            data=df.to_dict('records'),
+                     for i in dfg.columns],
+            data=dfg.to_dict('records'),
             page_size=10,
             style_cell=dict(textAlign='left'),
             style_header=dict(backgroundColor="paleturquoise"),
@@ -41,7 +51,7 @@ app.layout = html.Div(
     ]
 )
 
-app.run_server(debug=True)
+app.run_server(debug=False)
 
 if __name__ == "__main__":
-    munging
+    print('This is Main')
